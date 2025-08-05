9e6a16f99740ee24153856d79a784f5297593206
jareena2005 <jareenan42@gmail.com>
2025-08-05 19:37:00 +0530

    Removed env from version control and added to .gitignore

--- voice/voice/settings.py
+++ voice/voice/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'voice_1',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'voice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'voice_db',
+        'USER':'voiceuser_1',
+        'PASSWORD':'Maryam@2005',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
